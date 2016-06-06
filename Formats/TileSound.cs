a398ca4fd27d9f6e9bf234bd756592ddfd8027a4
DummkopfOfHachtenduden <dummkopfhachtenduden@gmail.com>
2016-06-06 03:05:19 +0200

    Added some types

---
+++ Formats/TileSound.cs
@@ -0,0 +1,17 @@
+public enum TileSound : int
+{
+    Dirt = 0,
+    Sand = 1,
+    Ashfield = 2,
+    Stone = 3,
+    Metal = 4,
+    Wood = 5,
+    Mud = 6,
+    Water = 7,
+    DeepWater = 8,
+    Snow = 9,
+    Grass = 10,
+    LongGrass = 11,
+    Forest = 12,
+    Cloud = 13
+}
